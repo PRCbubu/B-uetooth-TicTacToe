5e2d457b332d44779f2ddb8200286a062230d4c5
BabuRoyC <prcbubu@gmail.com>
2023-04-18 17:13:40 +0530

    Made the layouts of first page, the game

---
+++ app/src/main/java/com/example/bluetoothtictactoe/ReasultDialog.java
@@ -0,0 +1,16 @@
+package com.example.bluetoothtictactoe;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ReasultDialog extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reasult_dialog);
+    }
+}
